5a3d4e6141883463de6c349cfed58433caa92bc3
wangda8791 <wangda8791@163.com>
2019-08-26 03:47:52 +1000

    example added

---
+++ example/index.js
@@ -0,0 +1,5 @@
+require("arr-flat")
+
+console.log("[1,2,[3,4,[5]],6] => ", [1,2,[3,4,[5]],6].flatten());
+console.log("[1,2,3,4,5,6] => ", [1,2,3,4,5,6].flatten());
+console.log("[['a','b'],1,2,3] => ", [['a','b'],1,2,3].flatten());
